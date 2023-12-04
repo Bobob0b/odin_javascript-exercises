e4b8e52aded52f7644366e2850725d561f7f681b
Bobob0b <github@mzx.at>
2023-12-04 20:41:52 +0100

    ex. 02 finished!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let returnstring = "";
+
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            returnstring = returnstring + string;
+        }
+    } else {
+        returnstring = "ERROR";
+    }
+
+    return returnstring;
 
 };
 
