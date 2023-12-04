8d1143841865b42555b10f664841749b5ba931fe
Bobob0b <github@mzx.at>
2023-12-04 22:12:35 +0100

    ex. 04 finished!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+    let retArr = [];
+
+
+
+
+    for (const element of arr) {
+        if (!args.includes(element)){
+            retArr.push(element);
+        }
+    }
+    return retArr
 };
 
 // Do not edit below this line
