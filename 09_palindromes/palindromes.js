3d622e3960b47e318c1ec7bb2571a44a7aa2154f
Bobob0b <github@mzx.at>
2023-12-07 21:27:12 +0100

    ex 09 finished! bit hacky, solution is cleaner.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // sanitize string
+    let isPalindrome = true;
+    str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let arrStr = str.split("");
+    let arrReverse = [...arrStr];
+    arrReverse = arrReverse.reverse();
+    for (x = 0; x < arrStr.length; x++) {
+        if (arrStr[x] != arrReverse[x]) {
+            isPalindrome = false;
+            break;
+        }
+    }
 
+    return isPalindrome;
 };
 
 // Do not edit below this line
