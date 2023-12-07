bb6f7b982608f02209ca044d68b3f6a75ac459da
Bobob0b <github@mzx.at>
2023-12-07 22:02:38 +0100

    ex. 10 finished!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+    let n1 = 1; // read: n-1
+    let n2 = 1; // read: n-2
+    let sum = 0;
+
+    if (member < 0) return "OOPS";
+    if (member == 0) return 0;
+    if (member == 1 || member == 2) return n1;
+    if (member >= 3) {
+        for (let i = 3; i <= member; i++) {
+            sum = n1 + n2; // current value is the sum of the two previous values
+            // after building the sum every variable gets the value of the next one, beginning with the last.
+            n2 = n1;
+            n1 = sum;
+        };
+        return sum;
+    };
 
 };
 
