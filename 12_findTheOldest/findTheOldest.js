078ebb5012feb0f2255b8e9a8de8ec078bbd46f3
Bobob0b <github@mzx.at>
2023-12-07 23:11:41 +0100

    ex. 12 finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+
+    return array.sort((a, b) => {
+
+        let thisYear = new Date().getFullYear();
+        "yearOfDeath" in a ? a.currAge = a.yearOfDeath - a.yearOfBirth : a.currAge = thisYear - a.yearOfBirth;
+        "yearOfDeath" in b ? b.currAge = b.yearOfDeath - b.yearOfBirth : b.currAge = thisYear - b.yearOfBirth;
+
+        return (a.currAge > b.currAge) ? -1 : 1;
+
+
+    })[0];
+
 
 };
 
