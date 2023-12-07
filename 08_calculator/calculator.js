4c5dac87eb463cd4e13e7ba454e7af92b295ea54
Bobob0b <github@mzx.at>
2023-12-07 20:47:14 +0100

    ex. 08 finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((sum, item) => {
+    return sum + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((sum, item) => {
+    return sum * item;
+  });
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let sum = 1;
+  let i = 1;
+  while (i <= number) {
+    sum = sum * i;
+    i++;
+  };
+  return sum;
 };
 
 // Do not edit below this line
